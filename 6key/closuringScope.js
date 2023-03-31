b2967b1f15442be874097a3acc33f5545e4a83d3
0DiNEC <as.odinson@gmail.com>
2023-03-31 12:48:32 +0300

    feat: complite with chat gpt

---
+++ 6key/closuringScope.js
@@ -0,0 +1,14 @@
+function createFunctions(n) {
+  var callbacks = [];
+
+  for (let i=0; i<n; i++) {
+    callbacks.push(function() {
+      return i;
+    });
+  }
+  
+  return callbacks;
+}
+
+var callbacks = createFunctions(5);
+console.log(callbacks[1]())
