4ae4ea245bcdf13f9e2c10be69e3fe0526335016
0DiNEC <as.odinson@gmail.com>
2023-06-16 19:57:36 +0300

    refactor: replace code by eslint

--- 6keu/Clock.js
+++ 6keu/Clock.js
@@ -1,12 +1,11 @@
-let whatTimeIsIt = function (angle) {
-  if (angle < 30)
-    angle = 360 + angle;
-  let hourAngle = 30;
+const whatTimeIsIt = function (angle) {
+  if (angle < 30) { angle = 360 + angle; }
+  const hourAngle = 30;
   let hour = (Math.floor(angle / hourAngle)).toString();
-      hour = hour.length === 1 ? "0" + hour : hour;
-  let minut = angle % hourAngle === 0 ? "00" : (Math.floor(((angle % hourAngle) * 60) / hourAngle)).toString();
-      minut = minut.length === 1 ? "0" + minut : minut; 
-  return hour + ":" + minut;
-  }
+  hour = hour.length === 1 ? '0' + hour : hour;
+  let minut = angle % hourAngle === 0 ? '00' : (Math.floor(((angle % hourAngle) * 60) / hourAngle)).toString();
+  minut = minut.length === 1 ? '0' + minut : minut;
+  return hour + ':' + minut;
+}
 
 console.log(whatTimeIsIt(63.73435863870628));
--- 6keu/VowelOne.js
+++ 6keu/VowelOne.js
@@ -1,9 +1,8 @@
-function vowelOne(s){
+function vowelOne (s) {
   const vowel_list = 'aeiouAEIOU';
-  let anwser = "";
+  let anwser = '';
 
-  for (let i = 0; i < s.length; i++) 
-    anwser += vowel_list.indexOf(s[i]) !== -1? '1': '0';
-      
-    return anwser;
+  for (let i = 0; i < s.length; i++) { anwser += vowel_list.indexOf(s[i]) !== -1 ? '1' : '0'; }
+
+  return anwser;
 }
