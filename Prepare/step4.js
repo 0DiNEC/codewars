afa9f7565e190f4e1a55cc339fdc563ee9cd009a
Andrey <as.odinson@gmail.com>
2023-09-15 01:51:42 +0300

    refactor

--- Prepare/step4.js
+++ Prepare/step4.js
@@ -2,11 +2,11 @@
 
 if (!Function.prototype.bind) {
   Function.prototype.bind = function (context) {
-    var fn = this;
-    var args = Array.prototype.slice.call(arguments, 1);
+    let fn = this;
+    let args = Array.prototype.slice.call(arguments, 1);
 
     return function () {
-      var bindArgs = Array.prototype.slice.call(arguments);
+      let bindArgs = Array.prototype.slice.call(arguments);
       return fn.apply(context, args.concat(bindArgs));
     };
   };
