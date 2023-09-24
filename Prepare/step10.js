9810dc2b93f02c67e9ab9c7685614d4bc756f058
Andrey <as.odinson@gmail.com>
2023-09-25 01:54:58 +0300

    feat:

---
+++ Prepare/step10.js
@@ -0,0 +1,12 @@
+// add
+const add = function (a) {
+  return function (b) {
+    if (b) {
+      return add(a + b);
+    }
+    return a;
+  };
+};
+const result = add(5)(9);
+
+console.log(result());
