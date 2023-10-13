0f7fe4b0b9f9fd6a4aac0acf9ea9235b35fcd12d
Andrey <as.odinson@gmail.com>
2023-10-13 22:56:31 +0300

    feat: save null positions

---
+++ 3keu/Complete a matrix.js
@@ -0,0 +1,37 @@
+// We get
+// [ 10 ,null, 3  ],
+// [ 4  ,null, 16 ],
+// [null, 12 ,null]
+
+// the answer is
+// [10, 8, 3],
+// [ 4, 1,16],
+// [ 7,12, 2]
+
+function completeMatrix(matrix) {
+  const nullArrPosition = {
+    positions: [
+    /*{
+      row: 
+      col:
+    }*/
+    ],
+  };
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === null)
+        nullArrPosition.positions.push({ row: i, col: j });
+    }
+  }
+  
+  return null;
+}
+
+var matrix = [
+  [10, null, 3],
+  [4, null, 16],
+  [null, 12, null],
+];
+
+completeMatrix(matrix);
+
